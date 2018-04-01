2a049e589c3c72eab5881ab6f3caf839352802d7
Rashad595 <34640664+Rashad595@users.noreply.github.com>
2018-04-02 01:34:03 +0400

    1,2,3 Homework is ready.

---
+++ main.js
@@ -0,0 +1,46 @@
+
+//Verilən array-dəki rəqəmlərin hasilini tapan funksiya:
+
+// function ardi(a, b, c, ) {
+//     var x = [a, b, c,];
+//     var d = 1
+//     for (i = 0; i < x.length; i++) {
+//         d *= x[i]
+//     }
+//     alert("Arraydəki reqemlerin hasili " + d + " dir");
+// }
+// ardi(2, 5, 3, );
+
+
+// Verilən array-dəki rəqəmlərin kvadratlarının cəmini tapan funksiya:
+
+// function arsu(a, b, c) {
+//     var x = [a, b, c];
+//     var s = 0
+//     for (i = 0; i < x.length; i++) {
+//         s += (x[i] * x[i])
+//     }
+//     alert("Arraydəki rəqəmlərin kvadratlarının cəmi " + s + " dir");
+// }
+// arsu(1, 2, 2);
+
+
+//Verilən array-dəki cüt rəqəmlərin cəmini tapan funksiya:
+
+// function arsu(a, b, c) {
+//     var x = [a, b, c];
+//     var s = 0
+//     for (i = 0; i < x.length; i++) {
+//         if (x[i] % 2==0) {
+//             s += x[i]
+//         }
+//     }
+//     alert("Arraydəki cüt rəqəmlərin cəmi " + s + " dir");
+// }
+// arsu(2, 3, 6);
+
+
+
+
+
+
